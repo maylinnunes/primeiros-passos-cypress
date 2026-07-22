8675333cee2ae7709799211113458a23cb935c14
Maylin Nunes <maylinmartins@gmail.com>
2026-07-22 15:13:29 +0100

    Add page objetcs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,15 +1,16 @@
 import userData from '../fixtures/userData.json'
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js'
+import MenuPage from '../pages/menuPage.js'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
 
 describe('Orange HRM Tests', () => {
 
   const selectorsList = {
-    usernameField: "[name= 'username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb > .oxd-text",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: ".oxd-alert-content-text",
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+   
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
@@ -20,20 +21,20 @@ describe('Orange HRM Tests', () => {
   }
 
   it.only('User Info Update - Sucess', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.location('pathname', { timeout: 10000 }).should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type('FirstnameTest')
-    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
+
+    dashboardPage.checkDashboardPage()
+
+    menuPage.accessMyInfo()
+   
+    cy.get(selectorsList.firstNameField, { timeout: 10000 }).clear({ force: true }).type('FirstnameTest')
+    cy.get(selectorsList.lastNameField).clear({ force: true }).type('LastNameTest', { force: true })
     cy.get(selectorsList.genericField).eq(4).clear().type('Employee')
     cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
     cy.get(selectorsList.genericField).eq(6).clear().type('DriversLicenseTest')
     cy.get(selectorsList.genericField).eq(7).clear({ force: true }).type('2026-10-10')
-    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.dateCloseButton).first().click()
     cy.get(selectorsList.genericField).eq(8).clear().type('ssnNumberTest')
     cy.contains('button', 'Save').click()
    
